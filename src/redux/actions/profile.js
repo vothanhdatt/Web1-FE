406b3356a3761fea7bbe5e3864e3730cc0b69601
Vo Thanh Dat <vothanhdat.tdc2019@gmail.com>
2021-10-16 11:24:12 +0700

    done features: view profile

---
+++ src/redux/actions/profile.js
@@ -0,0 +1,22 @@
+//FEATUREPOST
+export const GET_PROFILE_REQUEST = "GET_PROFILE_REQUEST";
+export const GET_PROFILE_SUCCESS = "GET_PROFILE_SUCCESS";
+export const GET_PROFILE_FAILURE = "GET_PROFILE_FAILURE";
+
+export const getProfileRequest = (payload) => {
+  return {
+    type: GET_PROFILE_REQUEST,
+    payload: payload,
+  };
+};
+//UPDATE PROFILE
+export const UPDATE_PROFILE_REQUEST = "UPDATE_PROFILE_REQUEST";
+export const UPDATE_PROFILE_SUCCESS = "UPDATE_PROFILE_SUCCESS";
+export const UPDATE_PROFILE_FAILURE = "UPDATE_PROFILE_FAILURE";
+
+export const updateProfileRequest = (payload) => {
+  return {
+    type: UPDATE_PROFILE_REQUEST,
+    payload: payload,
+  };
+};
