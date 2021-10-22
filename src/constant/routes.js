21123958d23fbe77cda0c1bd50d002184044c470
Tranvanlap <Tranvanlap2069@gmail.com>
2021-10-22 16:57:33 +0700

    Done layout detailpost

--- src/constant/routes.js
+++ src/constant/routes.js
@@ -6,6 +6,7 @@ const routes = {
   Login: "/Login",
   Register: "/Register",
   Pass: "/Pass",
+  detailpost: "/detailpost",
 };
 
 export default routes;
--- src/features/index.js
+++ src/features/index.js
@@ -10,6 +10,7 @@ import CreatePost from "./CreatePost/CreatePost";
 import Login from "./Login/Login";
 import Register from "./Register/Register";
 import Pass from "./Pass/Pass";
+import detailpost from "./DetalPost/DetailPost";
 export default (
   <Switch>
     <Route exact path={route.homepage} component={homepage} />
@@ -19,5 +20,6 @@ export default (
     <Route exact path={route.Login} component={Login} />
     <Route exact path={route.Register} component={Register} />
     <Route exact path={route.Pass} component={Pass} />
+    <Route exact path={route.detailpost} component={detailpost} />
   </Switch>
 );
