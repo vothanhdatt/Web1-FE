7f378f72ed515fec7a75be849f73ea3967dd9371
Tranvanlap <Tranvanlap2069@gmail.com>
2021-12-04 15:51:39 +0700

    link

--- src/components/FeatureMember.js
+++ src/components/FeatureMember.js
@@ -1,6 +1,8 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import env from "../env";
+
 FeatureMember.propTypes = {
   first_name: PropTypes.string,
   last_name: PropTypes.string,
@@ -25,7 +27,10 @@ function FeatureMember(props) {
             key={user.id}
           >
             <div className="w-24 sm:w-46 lg:col-span-1 ">
-              <img className="w-20 h-20 rounded-lg" src={user.avatar} />
+              <img
+                className="w-20 h-20 rounded-lg"
+                src={env.URL_IMAGE + user.avatar}
+              />
             </div>
             {/* <Link to={ }> */}
             <span className="pt-1 overflow-y-auto font-sans text-xs font-bold whitespace-normal lg:text-base sm:text-sm lg:mt-6">
--- src/features/DetalPost/DetailPost.js
+++ src/features/DetalPost/DetailPost.js
@@ -5,6 +5,8 @@ import { useDispatch, useSelector } from "react-redux";
 import { getDetailPostRequest } from "../../redux/actions";
 import * as moment from "moment";
 import env from "../../env";
+import FeaturesPost from "../../components/FeaturePost";
+import { getRelatedPostRequest } from "../../redux/actions";
 
 /* GET detailPost
  *   Call api lấy chi tiết bài viết
@@ -22,53 +24,78 @@ function DetailPost(props) {
     );
   }, [props.match.params.id]);
 
+  /* Call api lấy bài viết Lien quan
+  Hiển thị ra giao diện
+ */
+  const getRelatedPost = useSelector(
+    (state) => state.getRelatedPostReducer.data
+  );
+  useEffect(() => {
+    dispatch(
+      getRelatedPostRequest({
+        post_id: props.match.params.id,
+      })
+    );
+  }, [props.match.params.id]);
+
   if (getDetailPost) {
     return (
       <div>
         <Header />
-        <div className="max-w-5xl mx-auto mb-7">
-          <div className="mt-14">
-            <div className="px-2 font-sans font-bold text-4xl h3">
-              {getDetailPost.name}
-            </div>
-            <div className="flex pl-2 my-10">
-              <div className="w-16 h-16 ">
-                <img
-                  className="w-16 border-2 border-blue-500 rounded-full h-16"
-                  src={getDetailPost.authorAvatar}
-                />
+        <div className="grid-cols-3 grid-rows-1 gap-0 mx-auto my-10 lg:grid max-w-5xl">
+          <div className="col-span-2">
+            <div className="mt-14">
+              <div className="px-2 font-sans font-bold text-4xl h3">
+                {getDetailPost.name}
               </div>
-              <div className="pl-2 mb-1 whitespace-normal text-sm  ">
-                <Link className="pr-2 font-bold text-blue-600 text-xl">
-                  {getDetailPost.authorFirstName} {getDetailPost.authorLastName}
-                </Link>
-                <i
-                  className="text-blue-600 fa fa-check-circle fa-2x"
-                  aria-hidden="true"
-                ></i>
-                <p className="pt-1 text-base">
-                  {moment(getDetailPost.created_at).format("DD/MM/YYYY")} -{" "}
-                  {getDetailPost.views} View
-                </p>
-              </div>
-            </div>
-            <div className="px-2 w-auto">
-              <img
-                className="mx-auto my-10 rounded-xl w-auto"
-                src={env.URL_IMAGE + getDetailPost.image}
-              />
-            </div>
-            <div className="px-2">
-              <div className="text-base text-justify sm:text-lg md:text-xl lg:text-lg xl:text-xl content">
-                <div className="px-2">
-                  <div
-                    className="text-base text-justify sm:text-lg md:text-xl lg:text-lg xl:text-xl content"
-                    dangerouslySetInnerHTML={{
-                      __html: getDetailPost.content,
-                    }}
-                  ></div>
+              <div className="flex pl-2 my-10">
+                <div className="w-16 h-16 ">
+                  <img
+                    className="w-16 border-2 border-blue-500 rounded-full h-16"
+                    src={env.URL_IMAGE + getDetailPost.authorAvatar}
+                  />
+                </div>
+                <div className="pl-2 mb-1 whitespace-normal text-sm  ">
+                  <Link className="pr-2 font-bold text-blue-600 text-xl">
+                    {getDetailPost.authorFirstName}{" "}
+                    {getDetailPost.authorLastName}
+                  </Link>
+                  <i
+                    className="text-blue-600 fa fa-check-circle fa-2x"
+                    aria-hidden="true"
+                  ></i>
+                  <p className="pt-1 text-base">
+                    {moment(getDetailPost.created_at).format("DD/MM/YYYY")} -{" "}
+                    {getDetailPost.views} View
+                  </p>
                 </div>
               </div>
+              <div className="px-2 w-auto">
+                <img
+                  className="mx-auto my-10 rounded-xl w-auto"
+                  src={env.URL_IMAGE + getDetailPost.image}
+                />
+              </div>
+              <div className="px-2">
+                <div className="text-base text-justify sm:text-lg md:text-xl lg:text-lg xl:text-xl content">
+                  <div className="px-2">
+                    <div
+                      className="text-base text-justify sm:text-lg md:text-xl lg:text-lg xl:text-xl content"
+                      dangerouslySetInnerHTML={{
+                        __html: getDetailPost.content,
+                      }}
+                    ></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div className="pl-2 mt-60 font-sans text-2xl font-bold h3">
+              Bài Viết Liên Quan
+            </div>
+            <div className="flex flex-row mb-1 overflow-auto lg:flex-col">
+              <FeaturesPost featurePost={getRelatedPost} />
             </div>
           </div>
         </div>
