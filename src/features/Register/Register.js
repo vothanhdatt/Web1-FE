df6673b0f2a6bbe76a511eb3f0aa2d2a716a8a02
Nguyễn Khắc Danh <80993371+NguyenKhacDanh@users.noreply.github.com>
2021-12-04 11:40:23 +0700

    commit change layout

--- src/features/Register/Register.js
+++ src/features/Register/Register.js
@@ -218,7 +218,7 @@ function Register() {
             </div>{" "}
             <div>
               <button type="submit" 
-              className={`bg-blue-500 py-2 px-36 text-sm text-white rounded border border-green focus:outline-none focus:border-green-dark text-center`}
+              className={`bg-blue-500 py-2 px-32 text-sm text-white rounded border border-green focus:outline-none focus:border-green-dark text-center`}
               >
                 Register </button>{" "}
             </div>{" "}
