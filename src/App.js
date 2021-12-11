5988cd691e0edd7f32866fd3eb85c5c58d413f4e
Vo Thanh Dat <vothanhdat.tdc2019@gmail.com>
2021-12-12 02:57:01 +0700

    done: Filter

--- src/App.js
+++ src/App.js
@@ -5,15 +5,36 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { useEffect, useState } from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import PropTypes from "prop-types";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
 
-function App() {
+function App(props) {
+  const { isLoading } = props;
+
   return (
-    <LoadingOverlay active={false} spinner>
+    <LoadingOverlay active={isLoading} spinner>
       <Router>
+        {/* <Header /> */}
         <div className="App">{RouteContainer}</div>
         <Footer />
       </Router>
     </LoadingOverlay>
   );
 }
-export default App;
+App.propTypes = {
+  isLoading: PropTypes.bool,
+};
+function mapStateToProps(state) {
+  return {
+    isLoading: state.CRUDPostReducer.isLoading,
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators({}, dispatch),
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
